9752470d7168c52e925aecce89da1a3dd43dc51e
Exo-Minus <judahmwesigwa@gmail.com>
2021-11-19 21:57:07 +0300

    Added Js

---
+++ Lala.js
@@ -0,0 +1,17 @@
+console.log(10/67+8979-808*68482);
+
+let BoxOne=1
+let BoxTwo=3
+let BoxThree=5
+let BoxFour=7
+let BoxFive=9
+
+console.log(BoxOne+BoxTwo+BoxThree+BoxFour+BoxFive)
+
+let firstname="Judah"
+let secondname="Mwesigwa"
+
+console.log(`${firstname} ${secondname}`)
+
+let answer=prompt("How old are you")
+alert(`Wow! You are ${answer}!! That so incredible!!`)
